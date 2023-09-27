120dc71546f4e4e53301975aac07d74cc4f272f9
Zsolesz <72067999+gazdi435@users.noreply.github.com>
2023-09-27 10:25:24 +0200

    Add files via upload

---
+++ Új mappa/script.js
@@ -0,0 +1,15 @@
+const URL = "https://api.adviceslip.com/advice";
+
+let idNumber = document.getElementById("idNumber");
+let advice = document.getElementById("advice");
+
+function updateAdvice(){
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        idNumber.innerText = data.slip.id
+        advice.innerText = data.slip.advice
+    })
+}
+
+updateAdvice()
